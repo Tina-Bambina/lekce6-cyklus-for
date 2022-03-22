773e56ece7380917c27a3539743b42f5c6a69b5d
Tina-Bambina <speleerska@gmail.com>
2022-03-22 19:34:08 +0100

    cisla 0 az 10 pomoci FOR i pomoci WHILE (DU)

--- main.js
+++ main.js
@@ -1,31 +1,79 @@
-// Použij výpis do konzole prohlížeče pomocí console.log('text')
-// Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
-
-
-// První příklad - vypiš vzestupně čísla od 0 do 10
+// UKOL Z HODINY
 console.log('První příklad - vzestupná řada 0 - 10');
 
-//TADY NAPIS CYKLUS
+for (let i=0; i<11; i=i+1) {
+     console.log(i)
+   }
 
-console.log('-------------------');
-
-
-
-// Druhý příklad - zkopíruj předchozí příklad
-// a vyzkoušej, jaký je rozdíl mezi <= a <
-// v podmínce uvnitř cyklu
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
-//TADY NAPIS CYKLUS
+for (let i=0; i<=11; i=i+1) {
+    console.log(i)
+   }
 
-console.log('-------------------');
 
-
-
-// Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
 
-console.log('-------------------');
+for (let i=10; i>=0; i=i-1) {
+    console.log(i)
+   }
+
+
+
+// DU - POMOCI WHILE
+
+console.log('První příklad - vzestupná řada 0 - 10');
+
+let a = 0;
+
+while (a<=10)
+   { 
+     console.log(a)
+     a++
+   }
+
+
+console.log('Druhý příklad - rozdíl mezi <= a <');
+
+let b = 0;
+while (b<10)
+   { 
+     console.log(b)
+     b++
+   }
+
+
+console.log('Třetí příklad - sestupná řada 10 - 0');
+
+let c = 11;
+while (c)
+   { 
+     c--
+     console.log(c)
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
